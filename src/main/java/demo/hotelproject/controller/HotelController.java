2087870e2fa0adc9c93c8874f7feb983d2c47037
ikoinaris <g.koinaris@hotmail.com>
2020-11-09 18:05:06 +0100

    HotelController updated with getAllHotels & properties updated with connection strings

--- src/main/java/demo/hotelproject/controller/HotelController.java
+++ src/main/java/demo/hotelproject/controller/HotelController.java
@@ -1,8 +1,12 @@
 package demo.hotelproject.controller;
 
+import demo.hotelproject.entity.Hotel;
 import demo.hotelproject.repository.HotelRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/hotels")
@@ -12,5 +16,11 @@ public class HotelController {
 
     public HotelController(HotelRepository hotelRepository) {
         this.hotelRepository = hotelRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Hotel> getAllHotels(){
+        List<Hotel> hotels = hotelRepository.findAll();
+        return hotels;
     }
 }
