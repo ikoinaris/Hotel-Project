3f8d48b6b70c7b95cfdc65f5967a4f835627f33a
ikoinaris <g.koinaris@hotmail.com>
2020-11-09 17:49:00 +0100

    Hotel project first commit

---
+++ src/main/java/demo/hotelproject/repository/HotelRepository.java
@@ -0,0 +1,9 @@
+package demo.hotelproject.repository;
+
+import demo.hotelproject.entity.Hotel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends MongoRepository<Hotel, String> {
+}
---
+++ src/main/java/demo/hotelproject/seeder/DbSeeder.java
@@ -0,0 +1,13 @@
+package demo.hotelproject.seeder;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DbSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
